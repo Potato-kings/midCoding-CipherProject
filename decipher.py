11e3e4e0ef0660ee4e264b7375a36dbfe91de6e6
Alan Rosario <alan.rosario.stu@cedarpark.org>
2026-03-24 19:39:11 -0700

    Alans cipher decipher

--- decipher.py
+++ decipher.py
@@ -1,14 +1,14 @@
-message = input("Enter your message: ")
+message = input("Enter encrypted message: ")
 shift = int(input("Enter the shift number: "))
 shift = shift % 26
-encoded = ""
+decoded = ""
 for char in message:
     if char.isalpha():
         charnum = ord(char)
-        newcharnum = charnum + shift
+        newcharnum = charnum - shift
         newchar = chr(newcharnum)
-        encoded += newchar 
+        decoded += newchar 
     else:
-        encoded += char
-print("Encoded message:", encoded)
+        decoded += char
+print("Decoded message:", decoded)
     
